c871ae6d11e34536867acabcd709a8c4c2eb8df0
Your Name <you@example.com>
2026-09-10 11:32:41 +0000

    Fix S3 backend bucket name

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-hotstar-sivaj12" # Replace with your actual S3 bucket name
+    bucket = "devsecops-hotstar-sivaji12" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
